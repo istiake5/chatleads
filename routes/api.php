d6024e43c0d195f019e921d9c58c12899252b8a3
istiake5 <novelistiakeahamed@gmail.com>
2020-06-16 01:20:04 +0600

    Jwt Package

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/movie', 'MovieController');
 Route::apiResource('/movie/{movie}/comment', 'CommentController');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
