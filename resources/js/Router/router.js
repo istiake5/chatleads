66d7f1efca555d275b72f12b7e78772604c38950
Istiake Ahamed Novel <novelistiakeahamed@gmail.com>
2020-06-17 00:14:37 +0600

    Login component

--- resources/js/Router/router.js
+++ resources/js/Router/router.js
@@ -2,6 +2,20 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
+
+import Login from "../components/login/Login";
+const routes = [
+    //{ path: "/", component: Parallex },
+    { path: "/login", component: Login },
+    // { path: "/logout", component: Logout },
+    // { path: "/signup", component: Signup },
+    // { path: "/category", component: CreateCategory },
+    // { path: "/forum", component: Forum, name: 'forum' },
+    // { path: "/ask", component: Create },
+    // { path: "/question/:slug", component: Read, name: 'read' },
+
+];
+
 const router = new VueRouter({
     routes, // short for `routes: routes`
     hashbang: false,
--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-card
-    color="grey lighten-4"
-    flat
-    height="200px"
-    tile
-  >
+
     <v-toolbar dense>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-toolbar-title>Chatleads</v-toolbar-title>
 
       <v-spacer></v-spacer>
+      <router-link to="/forum">
       <v-btn> Forum</v-btn>
+      </router-link>
       <v-btn>Ask Question</v-btn>
       <v-btn>Category</v-btn>
+      <router-link to="/login">
       <v-btn>Login</v-btn>
+      </router-link>
+      
     </v-toolbar>
-  </v-card>
+  
 </template>
 <script>
 export default {
